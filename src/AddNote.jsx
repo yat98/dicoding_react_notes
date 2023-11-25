0c85bd1139580fb3f1ce976885d99ecd816b404e
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-11-25 20:49:51 +0800

    update: title label in add note

--- src/AddNote.jsx
+++ src/AddNote.jsx
@@ -3,13 +3,15 @@ function AddNote() {
     <div className="note-add">
       <h2 className="note-add__title">Note :</h2>
       <div>
-        <label htmlFor="title">Title</label>
+        <div className="note-add__title-label">
+          <label htmlFor="title">Title</label>
+          <p className="note-add__textarea-character">Rest Character: 30</p>
+        </div>
         <input type="text" id="title" className="note-add__input"/>
       </div>
       <div>
         <label htmlFor="body">Body</label>
         <textarea cols="30" id="body" rows="10" className="note-add__textarea"></textarea>
-        <p className="note-add__textarea-character">Rest Character: 30</p>
       </div>
       <button type="submit" className="note-add__button-save">Save Note</button>
     </div>
