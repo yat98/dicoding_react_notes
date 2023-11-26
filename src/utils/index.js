69c0ce2af126fd141927563310146c01ecccb392
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-11-26 12:27:40 +0800

    update: showFormattedDate

--- src/utils/index.js
+++ src/utils/index.js
@@ -52,7 +52,7 @@ const showFormattedDate = (date) => {
     month: "long",
     day: "numeric"
   }
-  return new Date(date).toLocaleDateString("id-ID", options)
+  return new Date(date).toLocaleDateString("en-EN", options)
 }
 
 const notificationAlert = (title, text) => {
